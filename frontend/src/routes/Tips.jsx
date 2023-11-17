ea0097ffd396459106b308aa92c88f1056b56bea
Matt R <mattk.rose@gmail.com>
2023-11-16 17:06:39 -0800

    include template routing pages for Contact, Recipes, and Tips

---
+++ frontend/src/routes/Tips.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+const Tips = () => {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  );
+};
+
+export default Tips;
