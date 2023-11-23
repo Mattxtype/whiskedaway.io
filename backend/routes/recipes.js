979e6c70272a388af46a2f0eca608e62971ba76f
Matt R <mattk.rose@gmail.com>
2023-11-22 16:12:23 -0800

    implement get Latest recipes call in backend

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -7,4 +7,7 @@ const router = express.Router();
 // /recipe/add-recipe => POST
 router.post('/add-recipe', recipesController.postAddRecipe)
 
+// /recipe/get-latest-recipes => GET
+router.get('/get-latest-recipes', recipesController.getLatestRecipe);
+
 module.exports = router;
