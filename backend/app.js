ef2de06fe96353605fd472f04f87bb0d64313dec
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2023-11-29 13:18:58 -0800

    Dockerize app, including compose files, logs volume, and storing MongoDB information in enviroment variables

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -9,16 +13,24 @@ const recipeRoutes = require("./routes/recipes");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'logs', 'access.log'),
+  { flags: 'a' }
+);
+
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use('/recipe', recipeRoutes);
 
 mongoose
   .connect(
-    `mongodb+srv://MattKRose:wLq2LveGqBfB4I19@whiskedaway.0ieu3oc.mongodb.net/recipe`
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@whiskedaway.0ieu3oc.mongodb.net/recipe`
   )
   .then((result) => {
-    console.log("connected to database");
+    console.log("connected to MONGODB");
     app.listen(80);
   })
   .catch((err) => {
+    console.log("failed to connect to MONGODB")
     console.log(err);
   });
