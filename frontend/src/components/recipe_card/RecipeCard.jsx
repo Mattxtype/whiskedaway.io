dbda44e16797f47e2f91093da6e3bdbe96677ecc
Matt R <mattk.rose@gmail.com>
2023-12-05 17:42:23 -0800

    insert test image into recipe card

--- frontend/src/components/recipe_card/RecipeCard.jsx
+++ frontend/src/components/recipe_card/RecipeCard.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import pancakes from '../../assets/pancakes.jpg';
 
 const RecipeCard = ({imageId, title}) => {
   return (
     <div className='flex flex-col basis-60 bg-gray-200 m-5'>
-        <div className='text-center max-h-96 h-96'>{imageId}</div>
+        <div className='max-h-96 h-96'>
+          <img className='h-full' src={pancakes}></img>
+        </div>
         <div className='border border-black text-center'>
             <div className='text-xl font antialiased font-bold tracking-wide pt-2 overflow-visible'>{title}</div>
             <button>read more</button>
