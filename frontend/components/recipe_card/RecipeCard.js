f6b3f3a618b03d9f92d509a55de9f4dd3cc5715e
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2023-12-06 20:17:15 -0800

    fix image bug

--- frontend/components/recipe_card/RecipeCard.js
+++ frontend/components/recipe_card/RecipeCard.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import pancake from '../../public/pancakes.jpg'
 
 const RecipeCard = ({ imageId, title }) => {
   return (
     <div className="flex flex-col basis-60 bg-gray-200 m-5">
       <div className="max-h-96 h-96">
-        <img className="h-full" src='/pancakes.jpg'></img>
+        <Image className="h-full w-full" src={pancake}></Image>
       </div>
       <div className="border border-black text-center">
         <div className="text-xl font antialiased font-bold tracking-wide pt-2 overflow-visible">
